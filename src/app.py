28db6f5b2881e84b3264793a91e01d03c4f6d7e9
Silas HW <silas.hayes.williams@gmail.com>
2022-07-23 19:35:13 +0100

    Add lib.py

---
+++ src/app.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+
+class Interface(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs) # call the init function of the inherited class
+
+if __name__ == '__main__':
+    app = Interface()
+    app.mainloop()
