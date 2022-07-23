307e51320661cdd6e2a4855f7933352da61f6c93
Silas HW <silas.hayes.williams@gmail.com>
2022-07-23 19:38:29 +0100

    Add classes.py

---
+++ src/classes/classes.py
@@ -0,0 +1,13 @@
+# The main functionality of the project lies here. This includes:
+#   - Image preprocessing
+#   - OCR
+#   - Text-to-speech
+# Image capturing will be handled by the UI so don't worry about that
+
+class OCR:
+
+    def __init__(self):
+        pass
+
+    def scan_image(self, image):
+        pass
