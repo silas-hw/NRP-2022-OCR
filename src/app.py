cb2ee2838fa85798a616305d88c7c33f613a2aff
Silas HW <silas.hayes.williams@gmail.com>
2022-07-25 00:57:16 +0100

    Add scan method

--- src/app.py
+++ src/app.py
@@ -6,6 +6,9 @@ class Interface(tk.Tk):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs) # call the init function of the inherited class
 
+    def scan(self):
+        pass
+
 if __name__ == '__main__':
     app = Interface()
     app.mainloop()
