4c3af85ae01b1905d06756b7462d6abbd8cc1d47
Silas HW <silas.hayes.williams@gmail.com>
2022-07-25 13:38:49 +0100

    Add image capture

--- src/app.py
+++ src/app.py
@@ -1,10 +1,18 @@
 import tkinter as tk
 import tkinter.ttk as ttk
 
+import cv2 as cv2
+
+from PIL import Image, ImageTk
+
 class Interface(tk.Tk):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs) # call the init function of the inherited class
+        
+        ###################
+        # tkinter widgets #
+        ###################
 
         self.frame_main = tk.Frame(self)
 
@@ -47,6 +55,28 @@ class Interface(tk.Tk):
         self.check_saveaudio.grid(row=4, column=1)
 
         self.frame_settings.grid(row=0, column=1)
+
+        #################
+        # image capture #
+        #################
+
+        self.__video_feed = True
+        self.cv_cam = cv2.VideoCapture(0)
+        self.after(1, self.update_video)
+
+    def update_video(self):
+        if self.__video_feed:
+            result, img = self.cv_cam.read()
+
+            img_arr = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            img_pil = Image.fromarray(img_arr)
+            img_tk = ImageTk.PhotoImage(img_pil)
+
+            self.label_video.config(image=img_tk)
+            self.label_video.image = img_tk
+            
+            self.after(1, self.update_video)
+
 
     def scan(self):
         '''
